331fe4567a4853240bfcc7b80138466679d57a61
LienVTK1 <lienvtk1@fpt.com>
2026-01-14 21:37:04 +0700

    Get locators

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,15 +1,9 @@
 package base;
 
-import exercise1_2.elementLocators.ElementLocators;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 public class BaseTest {
 
@@ -19,7 +13,6 @@ public class BaseTest {
     public void getDriver() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
     }
 
     @AfterMethod
--- src/test/java/exercise1_2/tests/ElementLocatorsTest.java
+++ src/test/java/exercise1_2/tests/ElementLocatorsTest.java
@@ -27,6 +27,5 @@ public class ElementLocatorsTest extends BaseTest {
                 IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
