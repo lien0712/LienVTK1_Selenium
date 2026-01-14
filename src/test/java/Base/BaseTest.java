78d12c3b92dbaf8e1d1d0c1bf9ed0709feaa72c9
LienVTK1 <lienvtk1@fpt.com>
2026-01-14 15:26:12 +0700

    Change file name, method name, add Base package, coding conventions

---
+++ src/test/java/Base/BaseTest.java
@@ -0,0 +1,21 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void getDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        driver.quit();
+    }
+}
