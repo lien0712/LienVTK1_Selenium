97fd85baf6334d3aad5e89dd2cc8b4d499a56b48
LienVTK1 <lienvtk1@fpt.com>
2026-02-11 05:19:05 +0700

    fix ci error

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,8 +1,10 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.example.ConfigReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,6 +19,17 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        String isCI = System.getProperty("isCI", "false");
+        if (isCI.equals("true")) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+//        driver = new ChromeDriver(options);
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         con = new ConfigReader();
